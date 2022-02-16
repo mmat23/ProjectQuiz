2b8c30d7198e8268a113ee9e737125793e8f3528
mmat23 <matsock37@gmail.com>
2022-02-16 23:52:05 +0300

    cоздание каркаса

---
+++ src/test/java/tesis/webquiz/WebquizApplicationTests.java
@@ -0,0 +1,13 @@
+package tesis.webquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
